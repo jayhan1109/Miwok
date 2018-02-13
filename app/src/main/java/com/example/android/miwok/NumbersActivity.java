cb855c146db3e35f9494d87ff3cdbc655bc79e0c
Jason Han <jungho1109@gmail.com>
2018-02-13 23:09:02 +0900

    add Loop to add TextView to rootView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,6 +28,20 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        // find a root View of the whole layout
+        LinearLayout rootView= (LinearLayout)findViewById(R.id.rootView);
+
+        for(int i=0;i<words.size();i++) {
+            // Create a new TextView
+            TextView textView = new TextView(this);
+
+            // Set the current word to text View
+            textView.setText(words.get(i));
+
+            // Add this TextView to root view of this layout
+            rootView.addView(textView);
+        }
 
     }
 }
